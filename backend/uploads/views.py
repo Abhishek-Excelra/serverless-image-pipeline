538983e771eb89f0ae156a2fed1ed8f96786b923
Abhishek4298 <abhishekbhavsar143@gmail.com>
2026-03-22 02:43:39 +0530

    Clean git ignore and remove generated/tracked temp files

--- backend/uploads/views.py
+++ backend/uploads/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ImageUploadSerializer
 
-# Create your views here.
+class ImageUploadView(APIView):
+    def post(self, request):
+        serializer = ImageUploadSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+
+        return Response(serializer.errors, status=400)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,41 @@
-import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [file, setFile] = useState(null);
+
+  const handleUpload = async () => {
+    if (!file) return alert("Select file first");
+
+    const formData = new FormData();
+    formData.append("image", file);
+
+    try {
+      const res = await fetch("http://127.0.0.1:8000/api/upload/", {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await res.json();
+      console.log(data);
+      alert("Upload success");
+    } catch (err) {
+      console.error(err);
+      alert("Upload failed");
+    }
+  };
+
   return (
     <div>
-      <div className="card">
-        <h1>Image Upload</h1>
-        <hr />
-      </div>
-      <div className="card">
-        <input type="file" id="myfile" name="myfile" />
-      </div>
+      <h1>AWS Mastery 🚀</h1>
+
+      <input
+        type="file"
+        onChange={(e) => setFile(e.target.files[0])}
+      />
+
+      <button onClick={handleUpload}>
+        Upload Image
+      </button>
     </div>
   );
 }
