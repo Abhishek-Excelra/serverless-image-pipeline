536407ae36f246065d817a5889b2b802432dc808
Abhishek4298 <abhishekbhavsar143@gmail.com>
2026-03-22 01:51:30 +0530

    initial setup React and Django

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div className="card">
+        <h1>Image Upload</h1>
+        <hr />
+      </div>
+      <div className="card">
+        <input type="file" id="myfile" name="myfile" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
